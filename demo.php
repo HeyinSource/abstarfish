351ed8f59952a73b0f752063631a5fed873880de
lt <751971678@qq.com>
2021-10-22 11:06:26 +0800

    init first commit

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lt
+ * Date: 2021/10/22
+ * Time: 10:43
+ */
+require_once './vendor/autoload.php';
+use Abstarfish\Starfish\Tools;
+Tools::index();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lt
+ * Date: 2021/10/22
+ * Time: 11:03
+ */
+
+namespace Abstarfish\Starfish;
+
+
+class Tools
+{
+    public static function index()
+    {
+        echo '12313';
+    }
+}
