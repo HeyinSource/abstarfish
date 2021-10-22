271c69d1093fb7a060563ccac9975c52bdde1e89
lt <751971678@qq.com>
2021-10-22 14:58:03 +0800

    commit

---
+++ src/Abstarfish/Starfish/Test2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lt
+ * Date: 2021/10/22
+ * Time: 14:57
+ */
+
+namespace Abstarfish\Starfish;
+
+
+class Test2
+{
+    public static function Test()
+    {
+        echo 'test2 test';
+    }
+}
